e95aa44d35b84c0f7590f0cd12dc908367625bc1
manticore <manticore@manticore-linuxlab.(none)>
2011-10-03 12:31:47 +0700

    linux double list fix

--- glitchWrapper.cpp
+++ glitchWrapper.cpp
@@ -14,34 +14,42 @@ glitchWrapper::~glitchWrapper()
 
 void glitchWrapper::glitchEffect(int gWidth, int gHeight, unsigned char * gPixels, int gStart, int gAmount)
 {
-	FIMEMORY *GLITCH_MEM;
-	FIBITMAP *GLITCH_FUCK;
-    BYTE *GLITCH_DATA;
+    FIMEMORY *GLITCH_MEM;
+    FIBITMAP *GLITCH_FUCK;
+    FIBITMAP *GLITCH_SAVED;
+    BYTE *GLITCH_DATA = NULL;
+    DWORD GLITCH_SIZE;
 
-
- int GLITCH_SIZE;
  // Load image from ImageStruct to FreeImage Memory
  GLITCH_FUCK = FreeImage_ConvertFromRawBits((BYTE*)gPixels, gWidth, gHeight, 4 * gWidth, 24, 1, 2, 3, false);
+
+
+ 
+
  GLITCH_MEM  = FreeImage_OpenMemory();
  // Resave image as JPG to Memory
  FreeImage_SaveToMemory(FIF_JPEG, GLITCH_FUCK, GLITCH_MEM, 0);
- FreeImage_AcquireMemory(GLITCH_MEM, &GLITCH_DATA, (DWORD*) &GLITCH_SIZE);
+
+ //this one causing error on linux hmm...
+ FreeImage_AcquireMemory(GLITCH_MEM, &GLITCH_DATA, &GLITCH_SIZE);
+
+
 
  for(int g=gStart; g < (gStart+gAmount); g++  )
  {
 	GLITCH_DATA[g] = '\0'; 
  }
 
- FreeImage_SeekMemory(GLITCH_MEM, 0L, SEEK_SET);
- 
- FIBITMAP *GLITCH_SAVED = FreeImage_LoadFromMemory(FIF_JPEG, GLITCH_MEM, 0);  
 
+ FreeImage_SeekMemory(GLITCH_MEM, 0L, SEEK_SET);
+
+ GLITCH_SAVED = FreeImage_LoadFromMemory(FIF_JPEG, GLITCH_MEM, 0);  
+ 
+/* 
  int fWidth = FreeImage_GetWidth(GLITCH_SAVED); 
  int fHeight = FreeImage_GetHeight(GLITCH_SAVED); 
- 
- bool haspixels = FreeImage_HasPixels(GLITCH_SAVED);
- 
- if (haspixels){ 
+
+ if (FreeImage_HasPixels(GLITCH_SAVED)){ 
  	int bytespp = FreeImage_GetLine(GLITCH_SAVED) / FreeImage_GetWidth(GLITCH_SAVED);
  	for(int y = 0; y < fHeight; y++) {
  		BYTE *bits = FreeImage_GetScanLine(GLITCH_SAVED, y);
@@ -57,12 +65,12 @@ void glitchWrapper::glitchEffect(int gWidth, int gHeight, unsigned char * gPixel
  }else{
  	//post("FUX_GLITCH-> bitmap too fucked to be displayed...");
  }
-
+*/
 
  //lets experiment with this
- FreeImage_Unload(GLITCH_FUCK);	    
  FreeImage_Unload(GLITCH_SAVED);	    
+ FreeImage_Unload(GLITCH_FUCK);
  FreeImage_CloseMemory(GLITCH_MEM);
- free(GLITCH_DATA);
+ //free(GLITCH_DATA);
 
 }
